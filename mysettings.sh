73cd1423e1cd3f397fcaf3560bd1d3dabd43edf4
litong01 <litong01@users.noreply.github.com>
2020-02-21 01:32:22 -0500

    Create mysettings.sh

---
+++ mysettings.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+export PROJECT_NAME=greatweb
+export ENTITLEMENT_KEY=<<Your IBP Entitlement Key>>
+export EMAIL_ADDRESS=<<Your IBP registration email address>>
+export CONSOLE_PASSWORD=<<Pick a string as your IBP console password>>
+export IMAGE_SERVER=<<IBP image server>>
+export IMAGE_NAME=<<ibp-operator:2.1.2-20191217-amd64>>
+export STORAGE_CLASS=<<storage class name>>
+export DOMAIN_URL=<<domain url>>
